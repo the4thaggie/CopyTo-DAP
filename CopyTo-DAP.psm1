c1b84ec254b5bd846b971f5522bf1d5fa91c7ff9
The4thAggie <zach@the4thaggie.com>
2020-02-25 21:47:09 -0600

    Initial help

--- CopyTo-DAP.psm1
+++ CopyTo-DAP.psm1
@@ -1,3 +1,26 @@
+<#
+    .SYNOPSIS
+    Copy a file or folder to a MTP DAP
+
+    .PARAMETER MTPName
+    The name your DAP shows as in My Computer
+
+    .PARAMETER StorageRoot
+    The name of the storage card/SD/Internal as shown after double clicking DAP
+
+    .PARAMETER TargetFolder
+    Folder one level down from StorageRoot to drop files in (optional)
+
+    .PARAMETER PathToCopy
+    The file or folder you wish to copy into your DAP
+
+    .NOTES
+    This was made for Fiio M11 DAP but could be adapted
+
+    .EXAMPLE
+    CopyTo-DAP -MTPName 'Fiio M11' -StorageRoot 'M11 Micro SD2' -TargetFolder Lol -PathToCopy 'M:\Indie\AFolderOfMusic\' 
+
+#>
 Function CopyTo-DAP
 {
     [cmdletbinding()]
@@ -48,3 +71,5 @@ else
 
 $DestFolder.GetFolder.CopyHere($PathToCopy)
 }
+
+
